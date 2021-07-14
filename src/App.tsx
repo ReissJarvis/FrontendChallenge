51ac78e1ea586d946d32edb4cfdf72f3a5ddecf0
Reiss Jarvis <reissjarvis@etech.net>
2021-07-14 01:16:30 +0100

    Add API call to frontend service

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.scss';
 import { FilterContainer } from './components/filter-container/filter-container';
 import { WorkoutCard } from './components/workout-card';
+import { WorkoutApiService } from './services/workout-api.service';
+import { Workout } from './models/workout.model';
 
 
-const workoutTest = {
-        "id":"5c0fc25241403b024ad988aa",
-        "name":"Tricep Kickbacks",
-        "transcript":"<ol><li style=\"text-align: left;\">Place your right or left arm and leg along a flat bench and bend your torso forwards so it's parallel to the floor.</li><li style=\"text-align: left;\">In your free hand, hold a dumbbell up by your side. Your upper arm should be alongside your upper body with your lower arm pointing down to the floor. <b>First Position</b>.</li><li style=\"text-align: left;\">Contract your tricep and push the dumbbell back and up, until your arm is fully extended. Keep your upper arm in a fixed position throughout.<b> Second Position</b>.</li><li style=\"text-align: left;\">Slow and controlled, lower the dumbbell back into <b>First Position</b>.</li><li style=\"text-align: left;\">Finish your reps.</li><li style=\"text-align: left;\">Repeat for your other arm.</li></ol><p style=\"text-align: left;\">There's no place for egos in the gym. Always lift with a weight that's comfortable and controllable. If you experience any pain, put your safety first and stop.</p>",
-        "female":{
-            "image":"https://cdni.gs.lightning-e.com/media/5c0e6814ee0147fd16ef61d2-femalesinglearmkickbacksthumbnail.jpg"
-        },
-        "male":{
-            "image":"https://cdni.gs.lightning-e.com/media/5c0e516f72fc52b810eb7f57-malesinglearmkickbackthumbnail.jpg"
-        },
-        "bodyAreas":[
-            "Arms",
-            "Triceps"
-        ]
-    }
+const workoutApiService = new WorkoutApiService()
+
 function App() {
+
+    const [workouts, setWorkouts] = useState<Workout[]>([])
+    useEffect(() => {
+        workoutApiService.get()
+            .then(fetchedWorkouts => setWorkouts(fetchedWorkouts.exercises))
+    }, []);
+
   return (
    <div className="app-container">
      <div className="title-bar">
@@ -30,17 +26,12 @@ function App() {
          <FilterContainer/>
 
          <div className="workout-card-container">
-             <WorkoutCard workout={workoutTest} gender="female"/>
-             <WorkoutCard workout={workoutTest} gender="female"/>
-             <WorkoutCard workout={workoutTest} gender="female"/>
-             <WorkoutCard workout={workoutTest} gender="female"/>
-             <WorkoutCard workout={workoutTest} gender="female"/>
-             <WorkoutCard workout={workoutTest} gender="female"/>
-             <WorkoutCard workout={workoutTest} gender="female"/>
-             <WorkoutCard workout={workoutTest} gender="female"/>
-             <WorkoutCard workout={workoutTest} gender="female"/>
-             <WorkoutCard workout={workoutTest} gender="female"/>
-             <WorkoutCard workout={workoutTest} gender="female"/>
+
+             {
+                 workouts.map(wo =>
+                     <WorkoutCard workout={wo} gender="female"/>
+                 )
+             }
          </div>
 
      </div>
--- src/services/workout-api.service.tsx
+++ src/services/workout-api.service.tsx
@@ -1,1 +1,10 @@
-export class WorkoutApiService {}
+import { WorkoutApiResponse } from '../models/workout-api-response.model';
+import axios from 'axios';
+
+export class WorkoutApiService {
+
+    get(): Promise<WorkoutApiResponse> {
+        return axios.get<WorkoutApiResponse>('REPLACE_ME')
+            .then(result => result.data)
+    }
+}
